e1b7ff9550f2ddacb6c23b14493c55bfecaa065a
Peter Maas <pfmmaas@gmail.com>
2009-11-24 23:09:38 +0100

    changed declarations to call a method on the SQL val, reduces parentheses

--- src/test/scala/com/log4p/sqldsl/QueryBuilderSpec.scala
+++ src/test/scala/com/log4p/sqldsl/QueryBuilderSpec.scala
@@ -12,42 +12,42 @@ class QueryBuilderSpec extends Spec with ShouldMatchers {
 
   describe("A Query") {
     describe("(when containing nested 'and' or 'or' clauses)") {
-      val q = select ("*") from ("user") where (("name","peter") and (("active", true) or ("role", "admin")))
+      val q = SQL select 'all from ("user") where (("name","peter") and (("active", true) or ("role", "admin")))
       
       it("should contain parentheses at the correct locations in the resulting SQL") {
         q.sql should be ("select * from user where (name = 'peter' and (active = true or role = 'admin'))")
       }
     }
     describe("(when containing quotes in values)") {
-      val q = select ("*") from ("user") where (("name","p'eter"))
+      val q = SQL select "*" from ("user") where (("name","p'eter"))
 
       it("should escape those quotes in the resulting SQL") {
         q.sql should be ("select * from user where name = 'p''eter'")
       }
     }
     describe("(when testing against numeric value)") {
-      val q = select ("*") from("user") where (("id", 100))
+      val q = SQL select "*" from("user") where (("id", 100))
 
       it("shouldn't quote numbers in resulting SQL") {
         q.sql should be ("select * from user where id = 100")
       }
     }
     describe("(when containing 'in' clause)") {
-      val q = select ("*") from ("user") where (in("name","pe'ter","petrus"))
+      val q = SQL select "*" from ("user") where (in("name","pe'ter","petrus"))
 
       it("should generate in-clause with escaped values in SQL") {
         q.sql should be ("select * from user where name in ('pe''ter','petrus')")
       }
     }
     describe("(when ordered)") {
-      val q = select ("*") from ("user") where (("name","peter")) order Desc("name")
+      val q = SQL select "*" from ("user") where (("name","peter")) order Desc("name")
 
       it("should generate order SQL") {
         q.sql should be ("select * from user where name = 'peter' order by name desc")
       }
     }
     describe("(when asked to select 'all)") {
-      val q = select ('all) from ("user") where (("id", 100))
+      val q = SQL select 'all from ("user") where (("id", 100))
       
       it("should select *") {
         q.sql should be ("select * from user where id = 100")
@@ -59,14 +59,14 @@ class QueryBuilderSpec extends Spec with ShouldMatchers {
       }
     }
     describe("(when no where clause is specified)") {
-      val q = select ('all) from ("user")
+      val q = SQL select 'all from ("user")
 
       it("should select all rows without filter") {
         q.sql should be ("select * from user")
       }
     }
     describe("(when a where clause is not, but an order clause is specified)") {
-      val q = select ('all) from ("user") order Asc("name")
+      val q = SQL select 'all from ("user") order Asc("name")
 
       it("should select and order all rows without filter") {
         q.sql should be ("select * from user order by name asc")
